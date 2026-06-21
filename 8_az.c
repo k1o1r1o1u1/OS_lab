10b6ee4119cf871f5fe686acb8847fae755f3a3a
k1o1r1o1u1 <krishsteve909@gmail.com>
2026-06-21 11:37:17 +0530

    8_az done

---
+++ 8_az.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+
+    pid_t pid;
+    pid = fork();
+
+    if (pid == 0)
+    {
+        printf("[CHILD] This is child process.\n");
+        printf("[CHILD] My pid is %d.\n", getpid());
+        printf("[CHILD] My parent's pid is %d.\n", getppid());
+        printf("[CHILD] Exiting.....\n");
+    }
+    else
+    {
+        printf("[PARENT] This is parent process.\n");
+        printf("[PARENT] My pid is %d\n", getpid());
+        printf("[PARENT] My parent's pid is %d.\n", getppid());
+        printf("[PARENT] Sleeping for 10s....");
+
+        sleep(10);
+
+        printf("[PARENT] Child pid %d has ended but it has an entry in the process table.\n ", pid);
+        printf("[PARENT] It is a zombie process.\n");
+    }
+    return 0;
+}
